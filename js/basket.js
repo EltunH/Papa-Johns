885137202f59ae05ea9a90ecaa68c3a93d0b6cd4
EltunH <eltun.huseynov17@gmail.com>
2025-03-28 18:13:07 +0400

    change basket

--- js/basket.js
+++ js/basket.js
@@ -1,5 +1,4 @@
-const redCount = document.getElementById('redCount')
-const redCount1 = document.getElementById('redCount1')
+const redCount = document.querySelectorAll('.redCount')
 const spPrice = document.getElementById('spPrice')
 const spPrice1 = document.getElementById('spPrice1')
 const basketCount = document.getElementById('basketCount')
@@ -8,6 +7,7 @@ const favoruit = document.getElementById('favoruit')
 
 
 let basket = JSON.parse(localStorage.getItem('basket')) || []
+let sizeArr = ['S', 'M', 'L', 'XL', 'XXl', '3XL']
 
 function addBasket() {
     let yoxla = basket.find(item => item.id == DETAILDATA.id)
@@ -16,27 +16,34 @@ function addBasket() {
         basket.push(DETAILDATA)
     } else {
         basket.map(item => {
+            if(item.category == 'pizza') item.sizeValue = DETAILDATA.sizeValue
             if (item.id == DETAILDATA.id) item.productCount += count
         })
     }
     localStorage.setItem('basket', JSON.stringify(basket))
+    count = 1
     showBasket()
+    show(DETAILDATA)
+    changePizza(post, DETAILDATA.sizeValue)
 }
 
 function showBasket() {
     let qiymet = 0
     let umumi = 0
+    let ind = 0
     favoruit.innerHTML = ''
-    redCount.innerHTML = basket.length
-    redCount1.innerHTML = basket.length
+    redCount[0].innerHTML = basket.length
+    redCount[1].innerHTML = basket.length
     basketCount.innerHTML = basket.length
     basket.map((item, i) => {
+        if (item.category == 'pizza') ind = item.variations.findIndex(elm => elm.price == item.sizeValue)
         qiymet = (item.productCount * (item.sizeValue || item.price)).toFixed(1)
         favoruit.innerHTML += `
                     <div class="flex flex-col sm:flex-row justify-between items-center py-3 border border-slate-400 mt-2">
                         <div class="w-full sm:w-1/2 flex gap-2 items-center">
                             <img class="w-[50px]" src="${item.img}" alt="product" />
                             <h3 class="sm:text-[20px] font-bold">${item.title}</h3>
+                            <p class="sm:text-[20px] font-bold">${item.category == 'pizza' ? sizeArr[ind] : ''}</p>
                         </div>
                         <div class="w-full sm:w-1/2 justify-end flex items-center gap-3 px-2">
                             <div class="flex items-center sm:text-[20px] text-white p-2">
